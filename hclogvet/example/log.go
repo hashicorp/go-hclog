104da447a680bbeba124efd5df70a22f155a0167
Drew Bailey <abailey@hashicorp.com>
2021-04-01 11:32:50 -0400

    check interceptlogger statements

--- hclogvet/example/log.go
+++ hclogvet/example/log.go
@@ -8,6 +8,7 @@ import (
 
 func badHCLog() {
 	l := hclog.L()
+	il := hclog.NewInterceptLogger(&hclog.LoggerOptions{})
 
 	var (
 		err            = io.EOF
@@ -21,9 +22,18 @@ func badHCLog() {
 	l.Error("error opening file", "error", err)
 	l.Debug("too many connections", "connections", numConnections, "ip", ipAddr)
 
+	il.Info("ok", "key", "val")
+	il.Error("raft request failed", "error", err)
+	il.Error("error opening file", "error", err)
+	il.Debug("too many connections", "connections", numConnections, "ip", ipAddr)
+
 	// bad
 	l.Info("bad", "key")
 	l.Error("raft request failed: %v", err)
 	l.Error("error opening file", err)
 	l.Debug("too many connections", numConnections, "ip", ipAddr)
+	il.Info("bad", "key")
+	il.Error("raft request failed: %v", err)
+	il.Error("error opening file", err)
+	il.Debug("too many connections", numConnections, "ip", ipAddr)
 }
