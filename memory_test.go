fb7680a22c01404cbc1ad68ff1cf1e6483f14363
Evan Phoenix <evan@phx.io>
2024-03-05 15:39:09 -0800

    Add test for allocations

--- memory_test.go
+++ memory_test.go
@@ -25,3 +25,28 @@ func BenchmarkLoggerMemory(b *testing.B) {
 		}
 	}
 }
+
+func TestLoggerMemory(t *testing.T) {
+	var buf bytes.Buffer
+
+	logger := New(&LoggerOptions{
+		Name:   "test",
+		Output: &buf,
+		Level:  Info,
+	})
+
+	avg := testing.AllocsPerRun(100, func() {
+		logger.Trace("this is some message",
+			"name", "foo",
+			"what", "benchmarking yourself",
+		)
+
+		if buf.Len() != 0 {
+			panic("oops")
+		}
+	})
+
+	if avg != 0 {
+		t.Fatalf("ignored logs allocated")
+	}
+}
